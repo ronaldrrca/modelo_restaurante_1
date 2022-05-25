7a1ec6dc82a1e41c6516c6a024606c55f787ef5d
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-05-25 13:31:24 -0500

    Creando la estructura básica de archivos y carpetas

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reaturante</title>
+</head>
+<body>
+    <header>
+        <?php include './components/header.php' ?> 
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <?php include './components/footer.php' ?>
+    </footer>
+    <script src="./js/header.js"></script>
+</body>
+</html>
