93432e27f703d84a64b67f4aff1c82da0466bbb7
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-06-04 11:27:55 -0500

    Agregando el footer y la sección de contacto en el index

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+    <footer>
+        <div id="footer_section_a">
+            <img id="logo_footer" src="../assets/icons/logoV2.svg" alt="logo">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga harum nulla labore temporibus voluptas fugiat ad optio impedit amet eveniet enim culpa, commodi error ullam expedita animi dolorem reiciendis. Accusantium.</p>
+            <p id="accep_cc">ACEPTAMOS VISA Y MASTERCARD</p>
+            <div id="footer_creditCards">
+                <img class="creaditCard_logo" src="../assets/icons/visa_icon.svg" alt="logo de visa">
+                <img class="creaditCard_logo" src="../assets/icons/mastercard_icon.svg" alt="logo de mastercard">
+            </div>
+        </div>
+        <div id="footer_section_b">
+            <div id="footer_socialMedia">
+                <a href=""><img src="../assets/icons/linkedin_icon.svg" alt="icono linkedin"></a>
+                <a href=""><img src="../assets/icons/twitter_icon.svg" alt="icono twitter"></a>
+                <a href=""><img src="../assets/icons/instagram_icon.svg" alt="icono instagram"></a>
+                <a href=""><img src="../assets/icons/pinterest_icon.svg" alt="icono pinterest"></a>
+                <a href=""><img src="../assets/icons/youtube_icon.svg" alt="icono youtube"></a>
+            </div>
+            <p id="footer_copyRight">&copy Copyrights <span>Codews</span></p>
+        </div>
+    </footer>
+
+</html>
--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         </section>
         <section class="home_section">
             <p class="fancy_letter">Descubre</p>
-            <p class="home_titles">MENÚ POPULAR</p>
+            <h2 class="home_titles">MENÚ POPULAR</h2>
             <div class="star_lineContainer">
                 <img class="star_icon" src="./assets/icons/star_icon.svg" alt="icono de estrella">
                 <img class="star_icon" src="./assets/icons/star_icon.svg" alt="icono de estrella">
@@ -53,6 +53,19 @@
                 <div class="popularDish"><p class="popularDishes_name">Nombre del plato</p><img src="./assets/images/popular-menu-7.webp" alt="plato de comida" loading="lazy"></div>
                 <div class="popularDish"><p class="popularDishes_name">Nombre del plato</p><img src="./assets/images/popular-menu-8.webp" alt="plato de comida" loading="lazy"></div>
             </figure>
+        </section>
+        <section class="home_section mobile_component">
+            <h2>CONTACTO</h2>
+            <img class="home_contact_icon" src="./assets/icons/phone_icon_yellow.svg" alt="icono telefono">
+            <p class="home_contact_info">Teléfonos: +57 300-358-13-11</p>
+            <img class="home_contact_icon" src="./assets/icons/mail_icon_yellow.svg" alt="icono reloj">
+            <p class="home_contact_info">Email: ronaldrrca@gmail.com</p>
+            
+        </section>
+        <section class="home_section mobile_component">
+        <h2>HORARIO DE ATENCIÓN</h2>
+        <img class="home_contact_icon" src="./assets/icons/clock_icon_yellow.svg" alt="icono reloj">
+            <p class="home_contact_info">Lunes-Domingo: 10:00am - 10:00pm</p>
         </section>
         <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9717.039261135467!2d13.44081804803285!3d52.4925363424465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerl%C3%ADn%2C%20Alemania!5e0!3m2!1ses!2sco!4v1654293250763!5m2!1ses!2sco"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </main>
